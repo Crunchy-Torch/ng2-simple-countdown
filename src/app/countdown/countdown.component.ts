ed0ea73ea981b0d0030cec8c3825ad9402a7d9e2
DavidL <david.levayer@gmail.com>
2017-08-06 12:58:18 +0200

    Init simple counter component

---
+++ src/app/countdown/countdown.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-countdown',
+  templateUrl: './countdown.component.html',
+  styleUrls: ['./countdown.component.scss']
+})
+export class CountdownComponent implements OnInit {
+
+  time: moment.Moment;
+  sub: Subscription;
+  remaining: number;
+
+  @Input()
+  startTime = 60;
+
+  ngOnInit() {
+    this.reinit();
+  }
+
+  reinit() {
+    const m = moment().utcOffset(0);
+    m.set({ hour: 0, minute: 0, second: this.startTime, millisecond: 0 });
+    this.time = moment(m);
+    this.remaining = this.startTime;
+  }
+
+  start() {
+    this.sub = Observable.timer(0, 1000).subscribe(() => {
+      this.time.subtract(1, 'seconds');
+      this.remaining--;
+      if (this.remaining === 0) {
+        this.stop();
+      }
+    });
+  }
+
+  stop() {
+    this.sub.unsubscribe();
+  }
+}
