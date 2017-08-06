b2bcba52e9f6cfac623a464ea2ef5f60acb58057
DavidL <david.levayer@gmail.com>
2017-08-06 12:58:28 +0200

    Add demo to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { CountdownComponent } from './countdown/countdown.component';
+import { Component, ViewChild, OnInit, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChildren(CountdownComponent)
+  private countdowns: QueryList<CountdownComponent>;
+
+  firstCounter = 10;
+  secondCounter = 30;
+
+  ngAfterViewInit(): void {
+    this.countdowns.forEach(c => c.start());
+  }
 }
